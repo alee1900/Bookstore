c82c7d378eac9d87ca2755dfb2d93d0ec3988f76
Dragomir Alecsandra <dragomir.alecsandra19@gmail.com>
2021-09-27 16:09:03 +0300

    deleted useless lines

--- src/App.js
+++ src/App.js
@@ -6,20 +6,16 @@ import Login from "./pages/Login.js";
 import Signup from "./pages/Signup.js";
 import { AuthProvider } from "./Auth";
 import AddBook from "./pages/AddBook";
-// import PrivateRoute from "./PrivateRoute.js";
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
         <Navbar />
-        {/* <Switch> */}
-        {/* <PrivateRoute path="/" exact component={Home} /> */}
         <Route path="/" exact component={Login} />
         <Route path="/home" exact component={Home} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/addBook" exact component={AddBook} />
-        {/* </Switch> */}
       </Router>
     </AuthProvider>
   );
